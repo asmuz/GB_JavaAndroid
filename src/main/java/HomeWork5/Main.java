7d14f3a425650788f90847d454a8c100489bc8d0
AsmuZ <asmuz@mail.ru>
2021-03-17 15:09:02 +0300

    Tasks 1-3

--- src/main/java/HomeWork5/Main.java
+++ src/main/java/HomeWork5/Main.java
@@ -2,8 +2,20 @@ package HomeWork5;
 
 public class Main {
     public static void main(String[] args) {
-        Cat c = new Cat("Барсик");
-        c.run(150);
-        c.swim(50);
+        Cat c1 = new Cat("Барсик");
+        c1.run(150);
+        c1.swim(50);
+
+        Dog d1 = new Dog("Бобик");
+        d1.run(300);
+        d1.swim(50);
+
+        Cat c2 = new Cat("Мурзик");
+        c2.run(250);
+        c2.swim(2);
+
+        Dog d2 = new Dog("Тузик");
+        d2.run(600);
+        d2.swim(7);
     }
 }
