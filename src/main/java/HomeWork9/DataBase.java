fcf1dd26d0070ac5c49b9196167162f3c0d300f0
AsmuZ <asmuz@mail.ru>
2021-04-05 19:19:41 +0300

    Homework 9 Tasks 1-2

---
+++ src/main/java/HomeWork9/DataBase.java
@@ -0,0 +1,47 @@
+package HomeWork9;
+
+import java.sql.*;
+
+public class DataBase {
+    public static Connection connection;
+    public static Statement statement;
+    public static PreparedStatement addCat;
+
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:cats.db");
+        statement = connection.createStatement();
+    }
+
+    public static void disconnect() {
+        try {
+            if (statement != null) statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (addCat != null) addCat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static void add() throws SQLException {
+//        PreparedStatement sql = connection.prepareStatement("INSERT INTO cats(id, name, color, age, info) VALUES (?,?,?,?,?);");
+//    }
+
+    public static void addCat(Cat cat) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO cats(id, name, color, age, info) VALUES (?,?,?,?,?);");
+        ps.setInt(1, cat.getId());
+        ps.setString(2, cat.getName());
+        ps.setString(3, cat.getColor());
+        ps.setInt(4, cat.getAge());
+        ps.setString(5, cat.getInfo());
+        ps.executeUpdate();
+    }
+}
--- src/main/java/HomeWork9/Main.java
+++ src/main/java/HomeWork9/Main.java
@@ -8,6 +8,21 @@ package HomeWork9;
 
 public class Main {
     public static void main(String[] args) {
+        Cat[] cats = {
+                new Cat(1, "Барсик", "белый", 3, "Любит играть с клубком"),
+                new Cat(2, "Мурзик", "серый", 5, "Любит спать на коврике"),
+                new Cat(3, "Рыжик", "рыжий", 7, "Альфа самец")
+        };
 
+        try {
+            DataBase.connect();
+            for (int i = 0; i < cats.length; i++) {//добавляем котов из массива в базу данных
+                DataBase.addCat(cats[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DataBase.disconnect();
+        }
     }
 }
